f436fdfbd09e9e7913d2cb6c6a06d314293d576e
Maxim <zheemax@gmail.com>
2019-03-20 13:00:55 +0300

    Added mentor name and other lessons

--- app/src/main/java/com/epam/LessonsActivity.java
+++ app/src/main/java/com/epam/LessonsActivity.java
@@ -17,7 +17,55 @@ public class LessonsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lessons);
 
+        findViewById(R.id.lesson_android_kotlin_root_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLesson(UIComponentsActivity.class);
+            }
+        });
+        findViewById(R.id.lesson_android_recycler_root_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLesson(UIComponentsActivity.class);
+            }
+        });
+        findViewById(R.id.lesson_android_compound_view_root_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLesson(UIComponentsActivity.class);
+            }
+        });
+        findViewById(R.id.lesson_android_ui_root_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLesson(UIComponentsActivity.class);
+            }
+        });
         findViewById(R.id.lesson_components_root_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLesson(UIComponentsActivity.class);
+            }
+        });
+        findViewById(R.id.lesson_clean_code_root_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLesson(UIComponentsActivity.class);
+            }
+        });
+        findViewById(R.id.lesson_java_basics_root_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLesson(UIComponentsActivity.class);
+            }
+        });
+        findViewById(R.id.lesson_android_studio_tools_root_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLesson(UIComponentsActivity.class);
+            }
+        });
+        findViewById(R.id.lesson_github_root_view).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openLesson(UIComponentsActivity.class);
